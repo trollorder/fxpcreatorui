15cd8785493414a6c0016ede8f2cdb0b00a445e0
trollorder <73745120+trollorder@users.noreply.github.com>
2024-07-04 19:40:03 -0700

    mp3 generation ok

--- app/EditVideoDetails/[videoId]/page.js
+++ app/EditVideoDetails/[videoId]/page.js
@@ -8,6 +8,7 @@ export default function  EditVideoDetailsPage() {
     const [keyframesBase64, setKeyframesBase64] = useState(null);
     const [newTitle, setNewTitle] = useState('');
     const [videoDetails, setVideoDetails] = useState({});
+    const [audioBase64, setAudioBase64] = useState(null);
     const username = 'kelvin'
     
     async function getFirstKeyFrame(s3ObjectKeyNoVideo){
@@ -54,6 +55,17 @@ export default function  EditVideoDetailsPage() {
             }
             })
     }
+    function handleGetMP3(){
+        const s3ObjectKey = 'videos/'+s3ObjectKeyNoVideo;
+        axios.post(`${process.env.NEXT_PUBLIC_backendUrl}/generate-audio-for-transcript?username=${username}&s3ObjectKey=${s3ObjectKey}`)
+            .then((res) => {
+            const localAudioBase64 = res.data['audioBase64'];
+            setAudioBase64(localAudioBase64);
+            })
+            .catch((err) => {
+            console.log(err);
+            });
+    }
     
     useEffect(() => {
         var returnval = ''
@@ -77,21 +89,38 @@ export default function  EditVideoDetailsPage() {
             {keyframesBase64 && <img src={keyframesBase64[0]['imageBase64']} />}
             {videoDetails && <Typography variant='h6'>Video Title: {videoDetails['videoName']}</Typography>}
             <Typography variant='caption'>Edit Video Title</Typography>
+            
             <FormControl>
                 <Input placeholder='Enter New Video Title' required onChange={(e) => setNewTitle(e.target.value)} />
                 <Button variant='contained' color='primary' onClick={() => handleSubmit(newTitle)}>
                     Submit
                 </Button>
             </FormControl>
+
+            <Typography variant='caption'>Get MP3</Typography>
+            <Button variant='contained' color='primary' onClick={() => handleGetMP3()}>
+                Get MP3
+            </Button>
+            <audio controls key={audioBase64}>
+                <source src={audioBase64} type='audio/mpeg' />
+            </audio>
+
             <Typography>All Keyframes</Typography>
-            {keyframesBase64 && Object.keys(keyframesBase64).map((key) => (
+            {keyframesBase64 && Object.keys(keyframesBase64).map((key) => {
+                var isEditingLocal = false;
+                return(
                 <div key={key}>
                     <Typography variant='caption'>Keyframe {parseInt(key) + 1}</Typography>
                     <img src={keyframesBase64[key]['imageBase64']} />
                     <Typography variant='body2'>{keyframesBase64[key]['description']}</Typography>
-                    <Button variant='contained'>Edit Description</Button>
-                </div>
-            ))}
+                    <Button variant='contained' onChange={() => isEditingLocal = true}>Edit Description</Button>
+                    {isEditingLocal && <FormControl>
+                        <Input placeholder='Enter New Description' required />
+                        <Button variant='contained' color='primary'>Submit</Button>
+                    </FormControl>}
+                </div>)
+            })}
+            
         </div>
     );
 }
--- app/page.js
+++ app/page.js
@@ -12,21 +12,25 @@ export default function Home() {
   const [password, setPassword] = useState("");
   const [videos, setVideos] = useState([]);
   const [keyframes, setKeyframes] = useState({});
+  const [keyframesBase64, setKeyframesBase64] = useState({});
+  const [isReady, setIsReady] = useState(false);
   console.log(process.env.backendUrl)
   async function getAllVideos(){
     await axios.get(`${process.env.NEXT_PUBLIC_backendUrl}/list-videos?username=${username}`).then((res) => {
       setVideos(res.data.videos);
-      console.log(res.data.videos)
-      for (let i = 0; i < videos.length; i++) {
-        const video = videos[i];
-        getFirstKeyFrame(video['s3ObjectKey']).then((res) => {
-          setKeyframes({...keyframes, [video['s3ObjectKey']]: res.data['imageBase64']});
-          console.log(keyframes);
-        });
+      const videoIds = res.data.videos.map((video) => video['s3ObjectKey']);
+      for (const videoId of videoIds){
+        getFirstKeyFrame(videoId);
       }
-      
     });
   };
+
+  async function getFirstKeyFrame(s3ObjectKey){
+    await axios.get(`${process.env.NEXT_PUBLIC_backendUrl}/get-keyframe-for-video-base64?username=${username}&s3ObjectKey=${s3ObjectKey}&keyframeIndex=${0}`).then((res) => {
+      console.log(res.data);
+      setKeyframesBase64({...keyframesBase64, s3ObjectKey: res.data.imageBase64})
+    });
+  }
 
   
   useEffect(() => {
@@ -39,15 +43,15 @@ export default function Home() {
        videos.length>0 &&  videos.map((video) => (
             <div className="flex flex-col items-center justify-center w-full" key={video['s3ObjectKey']}> 
               <Typography>{video.videoName}</Typography>
-              <Button onClick={router.push('/EditVideoDetails')}>Edit Title</Button>
+              <Button onClick={() => router.push(`/EditVideoDetails/${video['s3ObjectKey'].split('/').pop()}`)}>Edit Title</Button>
             </div>
           ))
       }
-      {keyframes.length>0 && keyframes.map((keyframe) => (
-        <div className="flex flex-col items-center justify-center w-full" key={keyframe['s3ObjectKey']+'keyframe'}> 
-          <img src={keyframe['keyframe']} />
-        </div>
-      ))}
+      {
+        keyframesBase64.length>0 && Object.keys(keyframesBase64).map((key) => (
+          <img src={`${keyframesBase64[key]}`} alt="keyframe" />
+        ))
+      }
       <BottomBar />
     </main>
   );
